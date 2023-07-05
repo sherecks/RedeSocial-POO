84649eebde00c0d9543b0b6d98f74e1932bd285c
Joao Pedro Patricio do Nascimento <joaoppn00@gmail.com>
2023-07-05 10:19:40 -0300

    Test

--- UML-3.0-master/SRC/persistencia/Conexao.java
+++ UML-3.0-master/SRC/persistencia/Conexao.java
@@ -11,16 +11,21 @@ public class Conexao {
     private Conexao(){}
 
     public static Connection getConexao() throws ClassNotFoundException, SQLException{
+        try {
+            Class.forName("org.postgresql.Driver");
 
-        if(connection == null){
-            String url = "jdbc:postgresql://localhost:5432/RedeSocial";
-            String usuario = "postgres";
-            String senha = "1620";
-            connection = DriverManager.getConnection(url, usuario, senha);
+            if(connection == null){
+                String url = "jdbc:postgresql://localhost:5432/RedeSocial";
+                String usuario = "postgres";
+                String senha = "1620";
+                connection = DriverManager.getConnection(url, usuario, senha);
+            }
+
+            return connection;
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundException("Driver JDBC não encontrado.", e);
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao estabelecer a conexão com o banco de dados.", e);
         }
-
-        return connection;
     }
-
-    
 }
